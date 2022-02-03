32e9c6a136d07d5cae2435cacc32eee239c91436
Mariana Madeira <mrn.m.92@gmail.com>
2022-02-03 04:36:21 -0300

    Nuevo proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfilePicComponent } from './components/profile-pic/profile-pic.component';
+import { ButtonComponent } from './components/button/button.component';
+import { SeccionComponent } from './components/seccion/seccion.component';
+import { CardWideComponent } from './components/card-wide/card-wide.component';
+import { IconComponent } from './components/icon/icon.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    NavbarComponent,
+    ProfilePicComponent,
+    ButtonComponent,
+    SeccionComponent,
+    CardWideComponent,
+    IconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/card-wide/card-wide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-wide',
+  templateUrl: './card-wide.component.html',
+  styleUrls: ['./card-wide.component.css']
+})
+export class CardWideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
